f72751fdd35780b66d42c1931a5f0ec52ba3b3ed
ThofikhBisyron <sanada.ryuzaki1@gmail.com>
2024-08-27 11:56:39 +0700

    feat: add page Profile

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Home from './pages/Home'
+import Profile from './pages/Profile'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Home />
+  },
+  {
+    path: '/Profile',
+    element: <Profile/>
   }
 ])
 
