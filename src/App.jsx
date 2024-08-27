1abcc9d9628ff40ba446a89c0ff678071f9ef988
daffa <l200170028@student.ums.ac.id>
2024-08-27 11:50:49 +0700

    feat:page about us

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Home from './pages/Home'
+import About from './pages/About'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Home />
+  },
+  {
+    path: '/about-us',
+    element: <About />
   }
 ])
 
